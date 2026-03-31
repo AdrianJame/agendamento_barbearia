1f8108643a1428d3ee1ddb39344b9f709ad944e7
Matheusx21x <magmatheusf@gmail.com>
2026-03-30 21:38:15 -0300

    repositorio do barbeiro feito

---
+++ api/repository/barbeirorepository.js
@@ -0,0 +1,13 @@
+import conexao from "./connection";
+
+export async function LoginBarbeiros(email, senha) {
+    let sql = 'SELECT * FROM tb_barbeiro WHERE ds_email = ? AND ds_senha = ?';
+
+    let resposta = await conexao.query(sql, [email, senha]);
+    let linhas = resposta[0];
+    let barbeiro = linhas[0];
+
+    console.log(barbeiro);
+
+    return barbeiro;
+}
